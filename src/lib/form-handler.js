cf90bdf639a810663a5ded25b74a116ec5388dab
Trevor <trevor.geene@gmail.com>
2020-12-21 22:16:21 -0800

    Small fix

--- src/lib/form-handler.js
+++ src/lib/form-handler.js
@@ -1,19 +1,15 @@
 export const formSubmitHelper = (values, form_id, url) => {
   // Get Form Object
-  let outputDiv = document.querySelector('#' + form_id + ' > .output');
+  const form = document.getElementById(form_id);
+  let outputDiv = form.querySelector('.output');
 
   // Remove Old Form Output
-  if(outputDiv.hasChildNodes()) {
-    outputDiv.innerHtml = '';
-  }
+  outputDiv.innerHTML = '';
 
   // Set AJAX Variables
   const requestOptions = {
     method: 'post',
-    header: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: JSON.stringify(values)
+    body: new FormData(form)
   };
 
   // Make AJAX Request
