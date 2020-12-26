a5e3b4179619afb599daaefa6680295fdd28ef61
Trevor <trevor.geene@gmail.com>
2020-12-25 20:57:52 -0800

    Cononical Attribute

--- src/lib/misc.js
+++ src/lib/misc.js
@@ -2,6 +2,7 @@ export const update_meta_data = (title, description, url) => {
   // HTML Basics
   document.title = title;
   document.querySelector("meta[name='description']").setAttribute("content", description);
+  document.querySelector("link[rel='canonical']").setAttribute("href", 'https://djinn.dev' + url);
 
   // Social Media
   document.querySelector("meta[property='og:url']").setAttribute("content", 'https://djinn.dev' + url);
